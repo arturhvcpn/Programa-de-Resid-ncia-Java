e3f0d3e3001888a332397e08c69cdd14b927ec40
Artur Polo Norte <53799608+arturhvcpn@users.noreply.github.com>
2019-12-11 11:26:20 -0300

    markTen_Conta.java
    markTen_Conta.java

---
+++ markTen_Conta.java
@@ -0,0 +1,47 @@
+package Conta;
+
+public class Conta {
+    private int numero;
+    private String nomeCliente;
+    private double saldo;
+
+    public Conta(int numero, String nomeCliente,double saldo){
+        this.numero = numero;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public String getNomeCliente() {
+        return this.nomeCliente;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void deposito(double saldo){
+        this.saldo += saldo;
+    }
+    public void saque(double saldo){
+        this.saldo -=(saldo + 5);
+    }
+    public String toString(){
+        return ("Número da conta :: " + this.numero + " Nome do Cliente :: "+ this.nomeCliente + " Saldo R$" + this.saldo);
+    }
+}
