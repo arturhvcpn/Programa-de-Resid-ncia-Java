cb74dfb184369e0c48864d7b2b086b8305b7f820
Artur Polo Norte <53799608+arturhvcpn@users.noreply.github.com>
2020-01-20 11:23:04 -0300

    atualizacao

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
@@ -10,22 +11,21 @@ public class Reserva {
     private Date checkIn, checkOut;
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    //construtor
     public Reserva(int numeroQuarto, Date checkIn, Date checkOut){
         this.numeroQuarto = numeroQuarto;
         this.checkOut = checkOut;
         this.checkIn = checkIn;
     }
+
     public void atualizarDatas(Date checkIn, Date checkOut){
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
     public long duracao(){
-        long tempo = checkIn.getTime() - checkOut.getTime();
+        long tempo =  checkOut.getTime() - checkIn.getTime();
         long resultado = (TimeUnit.DAYS.convert(tempo,TimeUnit.MILLISECONDS));
         return resultado;
     }
-        // get
     public Date getCheckIn() {
         return checkIn;
     }
@@ -35,21 +35,41 @@ public class Reserva {
     public int getNumeroQuarto() {
         return numeroQuarto;
     }
-        // set
+
     public void setNumeroQuarto(int numeroQuarto) {
         this.numeroQuarto = numeroQuarto;
     }
 
+    public boolean setCheckIn(Date checkIn){
+            this.checkIn = checkIn;
+            if(checkIn.before(new Date())){
+                System.out.println("Erro: as datas devem ser futuras");
+            }else{
+                System.out.println(toString());
+            }
+            return true;
+    }
+
+    public boolean setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+        if(!checkOut.after(checkIn)){
+           System.out.println("Erro data informada posterior a data de hoje");
+        }else{
+            System.out.println(toString());
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
-        return  "Reserva" +
-                "\nNúmero do Quarto:" + numeroQuarto +
+        return  "\nReserva" +
+                "\nNúmero do Quarto: " + numeroQuarto +
                 "\nCheck In(dd/MM/yyyy): " + checkIn +
-                "\nCheck Out(dd/MM/yyyy):" + checkOut +
-                " Reserva: Quarto " + sdf.format(getNumeroQuarto()) +
+                "\nCheck Out(dd/MM/yyyy): " + checkOut +
+                "\nReserva: Quarto " + getNumeroQuarto() +
                 ", Check in " + sdf.format(getCheckIn()) +
                 ", Check out "+ sdf.format(getCheckOut()) +
                 ", "+ duracao() +
-                "noites";
+                " noites";
     }
 }
